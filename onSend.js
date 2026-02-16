26a964d0fc767fa8178e3939be5a9c4aa2b20847
vikash3pandey-sys <vikash3.pandey@paytm.com>
2026-02-16 15:50:32 +0530

    Update onSend.js

--- onSend.js
+++ onSend.js
@@ -1,12 +1,20 @@
 Office.onReady();
 
-var dialog;
-
 function checkExternalRecipients(event) {
     var item = Office.context.mailbox.item;
 
-    // Strictly check for paytm.com only
-    var trustedDomains = ["paytm.com"];
+    // 1. Guaranteed list of Trusted Domains
+    var trustedDomains = [
+        "paytmpayments.com", "paytmmoney.com", "paytminsurance.co.in", "paytmservices.com", "paytm.com", 
+        "powerplay.today", "inapaq.com", "paytmmall.io", "cloud.paytm.com", "firstgames.id", "ticketnew.com", 
+        "paytmmall.com", "paytmplay.com", "mobiquest.com", "fellowinfotech.com", "paytminsuretech.com", 
+        "alpineinfocom.com", "firstgames.in", "first.games", "paytmfoundation.org", "paytmforbusiness.in", 
+        "ps.paytm.com", "paytmcloud.in", "paytm.insure", "mypaytm.com", "paytm.business", "fincollect.in", 
+        "creditmate.in", "gamepind.com", "insider.paytm.com", "pmltp.com", "finmate.tech", "cdo.paytm.com", 
+        "paytmoffers.in", "paytmmloyal.com", "ocltp.com", "paytm.ca", "quarkinfocom.com", "pibpltp.com", 
+        "paytmfirstgames.com", "paytmgic.com", "paytmwholesale.com", "paytmlabs.com", "info.paytmfirstgames.com", 
+        "acumengame.com", "robustinfo.com", "one97.sg"
+    ];
 
     item.to.getAsync(function(result) {
         if (result.status !== Office.AsyncResultStatus.Succeeded) {
@@ -17,14 +25,16 @@ function checkExternalRecipients(event) {
         var recipients = result.value;
         var externalEmails = []; 
 
-        // 1. Check logic
+        // 2. Scan Logic
         for (var i = 0; i < recipients.length; i++) {
             var email = recipients[i].emailAddress.toLowerCase();
             var isSafe = false;
             
             for (var j = 0; j < trustedDomains.length; j++) {
-                if (email.indexOf("@" + trustedDomains[j]) > -1) {
-                    isSafe = true; break;
+                // Checks if it ends with exactly "@domain.com" or a subdomain ".domain.com"
+                if (email.endsWith("@" + trustedDomains[j]) || email.endsWith("." + trustedDomains[j])) {
+                    isSafe = true; 
+                    break;
                 }
             }
             if (!isSafe) { 
@@ -32,36 +42,38 @@ function checkExternalRecipients(event) {
             }
         }
 
-        // 2. Decision Logic
+        // 3. Decision Logic (Native Outlook Banner)
         if (externalEmails.length === 0) {
             // Internal Only -> Send Silently
             event.completed({ allowEvent: true });
         } else {
-            // External Found -> Open simplified popup window
-            var encodedEmails = encodeURIComponent(externalEmails.join(","));
-            var url = "https://vikash3pandey-sys.github.io/outlook-alerts/warning.html?ext=" + encodedEmails;
+            // External Found -> Use Native Outlook Banner instead of a Pop-up window
+            item.loadCustomPropertiesAsync(function (propResult) {
+                var props = propResult.value;
+                var warningStatus = props.get("WarningBypass"); 
 
-            // Open dialog
-            Office.context.ui.displayDialogAsync(url, { height: 40, width: 35, displayInIframe: true },
-                function (asyncResult) {
-                    if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-                        event.completed({ allowEvent: false, errorMessage: "Security Check Failed." });
-                    } else {
-                        dialog = asyncResult.value;
+                if (warningStatus === "yes") {
+                    // USER CLICKED SEND A SECOND TIME -> Let the email go through
+                    props.remove("WarningBypass");
+                    props.saveAsync(function() {
+                         event.completed({ allowEvent: true });
+                    });
+                } else {
+                    // FIRST CLICK -> Stop the send and show the native text banner at the top
+                    props.set("WarningBypass", "yes");
+                    props.saveAsync(function() {
                         
-                        // Wait for user to click Yes or No
-                        dialog.addEventHandler(Office.EventType.DialogMessageReceived, function(arg) {
-                            dialog.close(); 
-                            
-                            if (arg.message === "allow") {
-                                event.completed({ allowEvent: true }); // Send it
-                            } else {
-                                event.completed({ allowEvent: false }); // Cancel send
-                            }
+                        // Set the text for the native banner
+                        var recWord = externalEmails.length === 1 ? "recipient" : "recipients";
+                        var bannerText = "Confidentiality Warning: You are sending to " + externalEmails.length + " external " + recWord + ". Click Send again to confirm.";
+                        
+                        event.completed({ 
+                            allowEvent: false, 
+                            errorMessage: bannerText
                         });
-                    }
+                    });
                 }
-            );
+            });
         }
     });
 }
